9c7905adc492f35aa4d287a99f8bc636d387c84e
LucianaSerrao <luliserrao@eng.ci.ufpb.br>
2019-12-02 13:12:03 -0300

    Selection Sort

--- selectionSort.py
+++ selectionSort.py
@@ -1,15 +1,13 @@
 def selectionSort(A):
     for i in range(len(A)):
 
-        # Find the minimum element in remaining
-        # unsorted array
+        # Acha o elemento mínimo no array
         min = i
         for j in range(i + 1, len(A)):
             if A[min] > A[j]:
                 min = j
 
-        # Swap the found minimum element with
-        # the first element
+        # Troca o elemento minimo com o primeiro elemento do array
         A[i], A[min] = A[min], A[i]
 
     return A
